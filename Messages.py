b149888fe61672fef3ca737666e90bb9220c4463
julien <jusylvai@42paris.fr>
2024-04-05 17:11:29 +0200

    Object-oriented hallvoice

---
+++ Messages.py
@@ -0,0 +1,121 @@
+import datetime
+import os
+import json
+import redis
+from io import BytesIO
+from random import choice, randint
+from gtts import gTTS, gTTSError
+from pygame import mixer
+
+
+class Messages(object):
+    def __init__(self, conf, api):
+        self.redis = redis.Redis(host='127.0.0.1', port=6379, db=0)
+        self.welcomeMsg = conf.getWelcome()
+        self.goodbyeMsg = conf.getGoodbye()
+        self.buildingName = conf.getBuilding()
+        self.api = api
+        mixer.init()
+
+    def processMessage(self, msg):
+        data = json.loads(msg)
+        if data["firstname"] == "chantier":
+            return
+        if self.buildingName != data["building"]:
+            return
+        print(f"[{datetime.datetime.now()}] NEW MESSAGE: {msg}")
+        kind = data['kind']
+        login = data['login']
+        firstname = ""
+        if login is not None and login != "":
+            firstname = self.api.getUsualName(login)
+        if firstname is None or firstname == "":
+            firstname = data["firstname"]
+        jsonFile = "custom/" + login + ".json"
+        if os.path.isfile(jsonFile):
+            print(f"[{datetime.datetime.now()}] Custom HallVoice for " + login)
+            self.playCustomSound(kind, jsonFile, firstname)
+            return
+        else:
+            self.genericMessage(firstname, kind)
+
+    def playCustomSound(self, kind, jsonFile, firstname):
+        kind = "welcome" if kind == "in" else "goodbye"
+        try:
+            with open(jsonFile, 'r') as custom_file:
+                j = json.loads(custom_file.read())
+                if kind in j:
+                    if "mp3" in j[kind]:
+                        if os.path.isdir("mp3/" + j[kind]["mp3"]) is True:
+                            mixer.music.load("mp3/" + j[kind]["mp3"] + "/" + choice(os.listdir("mp3/" + j[kind]["mp3"])))
+                        elif os.path.isfile("mp3/" + j[kind]["mp3"]) is True:
+                            mixer.music.load("mp3/" + j[kind]["mp3"])
+                        mixer.music.play()
+                        while mixer.music.get_busy():
+                            pass
+                    elif "txt" in j[kind]:
+                        lang = j[kind]["lang"] if "lang" in j[kind] else "fr"
+                        self.say(j[kind]["txt"], lang)
+                else:
+                    self.genericMessage(firstname, kind)
+        except FileNotFoundError as e:
+            print(f"Custom HallVoice for {firstname} not found:\n{e}")
+
+    def genericMessage(self, firstname, kind):
+        tts = ""
+        if kind == "welcome" or kind == "in":
+            tts = self.welcomeMsg[randint(0, len(self.welcomeMsg) - 1)][1].replace("<name>", firstname)
+        elif kind == "goodbye" or kind == "out":
+            tts = self.goodbyeMsg[randint(0, len(self.goodbyeMsg) - 1)][1].replace("<name>", firstname)
+        print(f"[{datetime.datetime.now()}] {tts}")
+        self.say(tts, "fr")
+
+    def say(self, txt, lang):
+        mp3_fp = BytesIO()
+        if txt is not None and txt != "":
+            cache = self.redis.get(txt+lang)
+            if cache:
+                print(f"[{datetime.datetime.now()}] TTS cache getted!")
+                mp3_fp.write(cache)
+                mp3_fp.seek(0)
+                self.playMP3(mp3_fp)
+            else:
+                print(f"[{datetime.datetime.now()}] TTS cache not found, putting in cache")
+            try:
+                # Generate speech using gTTS and save to a BytesIO object
+                tts = gTTS(text=txt, lang=lang)
+                # Create and write the TTS to a BytesIO
+                mp3_fp = BytesIO()
+                tts.write_to_fp(mp3_fp)
+                # Convert the MP3 BytesIO object to WAV format in memory
+                mp3_fp.seek(0)  # Reset the file pointer to the beginning
+                self.redis.set(txt+lang, mp3_fp.read())
+                mp3_fp.seek(0)
+                self.playMP3(mp3_fp)
+            except gTTSError as e:
+                print(f"HallvoiceERROR TTS error:\n{e}")
+        else:
+            print(f"[{datetime.datetime.now()}] Cannot TTS, var txt is None")
+            cache = self.redis.get("HallvoiceERROR")
+            if cache:
+                print("HallvoiceERROR TTS cached")
+                self.playMP3(cache)
+            else:
+                print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS not cached, caching him")
+                try:
+                    tts = gTTS(text="Hallvoice Error, please contact staff member", lang="en")
+                    tts.write_to_fp(mp3_fp)
+                    # Convert the MP3 BytesIO object to WAV format in memory
+                    mp3_fp.seek(0)  # Reset the file pointer to the beginning
+                    self.redis.set("HallvoiceERROR", mp3_fp.read())
+                    mp3_fp.seek(0)
+                    self.playMP3(mp3_fp)
+                except gTTSError as e:
+                    print(f"HallvoiceERROR TTS error:\n{e}")
+
+    @staticmethod
+    def playMP3(mp3):
+        mixer.music.load(mp3)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            pass
--- porte.py
+++ porte.py
@@ -1,154 +1,17 @@
-import configparser
-import json
-import os
 import sys
-from confluent_kafka import Consumer, KafkaException, KafkaError, OFFSET_END
-from random import *
-from playsound import playsound
-from gtts import gTTS
-
-
-def say(txt, lang):
-    tts = gTTS(text=txt, lang=lang)
-    tts.save("/tmp/tts.mp3")
-    playsound("/tmp/tts.mp3")
-    os.remove("/tmp/tts.mp3")
-
-
-def read_config(file_path):
-    try:
-        config = configparser.ConfigParser()
-        config.read(file_path)
-
-        # Accessing values from the configuration file
-        kafka_servers = config.get("kafka_conf", "kafka_servers")
-        topic = []
-        topic.append(config.get("kafka_conf", "topic"))
-        group_id = config.get("kafka_conf", "group_id")
-        username = config.get("kafka_conf", "username")
-        password = config.get("kafka_conf", "password")
-        building = config.get("building", "name")
-        welcome = []
-        goodbye = []
-        for welcomeMsg in config.items("welcome"):
-            welcome.append(welcomeMsg)
-        for goodbyeMsg in config.items("goodbye"):
-            goodbye.append(goodbyeMsg)
-        return kafka_servers, topic, group_id, username, password, building, welcome, goodbye
-    except Exception as err:
-        print(f"Cannot open config.ini or error while reading config.ini:\n{err}")
-
-
-def create_consumer(kafka_servers, topic, group_id, username, password):
-    # Consumer configuration
-    conf = {
-        'bootstrap.servers': kafka_servers,
-        'group.id': group_id,
-        'security.protocol': 'SASL_SSL',
-        'sasl.mechanism': 'SCRAM-SHA-512',
-        'sasl.username': username,
-        'sasl.password': password,
-        'auto.offset.reset': 'latest',
-        'enable.auto.commit': True
-    }
-
-    # Create Kafka consumer instance
-    consumer = Consumer(conf)
-
-    # Go to last message
-    def my_assign(consumer, partitions):
-        for p in partitions:
-            p.offset = OFFSET_END
-        print('assign', partitions)
-        consumer.assign(partitions)
-
-    # Subscribe to the topic
-    consumer.subscribe(topic, on_assign=my_assign)
-    return consumer
-
-
-def consume_messages(consumer, building, welcome, goodbye):
-    try:
-        while True:
-            # Poll for messages
-            msg = consumer.poll(1.0)
-
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == KafkaError.PARTITION_EOF:
-                    # End of partition event
-                    sys.stderr.write(
-                        '%% %s [%d] reached end at offset %d\n' % (msg.topic(), msg.partition(), msg.offset()))
-                elif msg.error():
-                    raise KafkaException(msg.error())
-            else:
-                # Process the message
-                processMessage(msg.value().decode('utf-8'), building, welcome, goodbye)
-    except KeyboardInterrupt:
-        # Handle keyboard interrupt
-        pass
-    finally:
-        # Close down consumer to commit final offsets.
-        consumer.close()
-
-
-def playCustomSound(kind, jsonFile):
-    io = "welcome" if kind == "in" else "goodbye"
-    with open(jsonFile, 'r') as custom_file:
-        j = json.loads(custom_file.read())
-        if "mp3" in j[io]:
-            if os.path.isdir("mp3/" + j[io]["mp3"]) is True:
-                playsound("mp3/" + j[io]["mp3"] + "/" + choice(os.listdir("mp3/" + j[io]["mp3"])))
-            elif os.path.isfile("mp3/" + j[io]["mp3"]) is True:
-                playsound("mp3/" + j[io]["mp3"])
-        elif "txt" in j[io]:
-            lang = j[io]["lang"] if "lang" in j[io] else "fr"
-            say(j[io]["txt"], lang)
-
-
-def genericWelcome(firstname, welcomeMsg):
-    tts = welcomeMsg[randint(0, 7)][1].replace("<name>", firstname)
-    print(tts)
-    say(tts, "fr")
-
-
-def genericGoodbye(firstname, goodbyeMsg):
-    tts = goodbyeMsg[randint(0, 7)][1].replace("<name>", firstname)
-    print(tts)
-    say(tts, "fr")
-
-
-def processMessage(msg, buildingName, welcomeMsg, goodbyeMsg):
-    print("NEW MESSAGE: " + msg)
-    data = json.loads(msg)
-    if buildingName != data["building"]:
-        return
-    kind = data['kind']
-    firstname = data['firstname']
-    login = data['login']
-
-    jsonFile = "custom/" + login + ".json"
-    if os.path.isfile(jsonFile):
-        print("Custom HallVoice for " + login)
-        playCustomSound(kind, jsonFile)
-        return
-    else:
-        genericWelcome(firstname, welcomeMsg) if kind == "in" else genericGoodbye(firstname, goodbyeMsg)
+from Conf import Conf
+from API42 import API42
+from Kafka import Kafka
 
 
 if __name__ == "__main__":
-    config_file_path = 'config.ini'
-    try:
-        (kafka_servers,
-         topic,
-         group_id,
-         username,
-         password,
-         building,
-         welcome,
-         goodbye) = read_config(config_file_path)
-        consumer = create_consumer(kafka_servers, topic, group_id, username, password)
-        consume_messages(consumer, building, welcome, goodbye)
-    except Exception as err:
-        print(f"Error reading/opening configuration file: {err}")
+    if sys.argv[1] is not None:
+        try:
+            conf = Conf(sys.argv[1])
+            api = API42(conf)
+            consumer = Kafka(conf, api)
+            consumer.consume_messages()
+        except FileNotFoundError as err:
+            print(f"Error reading/opening configuration file: {err}")
+    else:
+        print("Where conf file? Use me like this!\npython3 porte.py config.yaml")
