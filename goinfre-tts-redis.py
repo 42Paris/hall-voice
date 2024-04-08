ce48498f7a7c5700a7b459d2c2b98cb4def7d871
julien <jusylvai@42paris.fr>
2024-04-09 01:18:17 +0200

    much better code, require list of firstname

--- goinfre-tts-redis.py
+++ goinfre-tts-redis.py
@@ -1,86 +1,35 @@
-import configparser
 import datetime
 import time
 from io import BytesIO
 import redis
-import threading
 from gtts import gTTS
-from pygame import mixer
-
-mixer.init()
+from Conf import Conf
 
 
-with open("config.ini", "r"):
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    apiUID = config.get("42api", "uid")
-    apiSEC = config.get("42api", "secret")
-    welcome = []
-    goodbye = []
-    for welcomeMsg in config.items("welcome"):
-        welcome.append(welcomeMsg)
-    for goodbyeMsg in config.items("goodbye"):
-        goodbye.append(goodbyeMsg)
+def api_call(name: str, Messages):
+    for m in Messages:
+        hallvoice = m[1].replace("<name>", name)
+        tts_cache = r.get(hallvoice + "fr")
+        if tts_cache:
+            print(f"[{datetime.datetime.now()}] TTS already in cache for: {hallvoice}")
+        else:
+            time.sleep(1)
+            # Generate speech using gTTS and save to a BytesIO object
+            tts = gTTS(text=hallvoice, lang="fr")
+            # Create and write the TTS to a BytesIO
+            mp3_fp = BytesIO()
+            tts.write_to_fp(mp3_fp)
+            # Convert the MP3 BytesIO object to WAV format in memory
+            mp3_fp.seek(0)  # Reset the file pointer to the beginning
+            print(f"[{datetime.datetime.now()}] TTS cached for: {hallvoice}")
+            r.set(hallvoice + "fr", mp3_fp.read())
 
 
-def api_call(start_l, end_l):
-    with open("login.list", "r") as f:
-        for i, line in enumerate(f):
-            if i < start_l:
-                continue
-            elif i >= end_l:
-                break
-            else:
-                firstname_cache = r.get(f"login: {line.strip()}").decode("utf-8")
-                if firstname_cache is not None:
-                    for v in welcome:
-                        txt = v[1].replace("<name>", firstname_cache)
-                        tts_cache = r.get(txt + "fr")
-                        if tts_cache:
-                            print(f"[{datetime.datetime.now()}] TTS cache getted for: {txt}")
-                        else:
-                            print(f"[{datetime.datetime.now()}] TTS cache not found for: {txt}")
-                            # Generate speech using gTTS and save to a BytesIO object
-                            tts = gTTS(text=txt, lang="fr")
-                            # Create and write the TTS to a BytesIO
-                            mp3_fp = BytesIO()
-                            tts.write_to_fp(mp3_fp)
-                            # Convert the MP3 BytesIO object to WAV format in memory
-                            mp3_fp.seek(0)  # Reset the file pointer to the beginning
-                            r.set(txt + "fr", mp3_fp.read())
-                    for v in goodbye:
-                        txt = v[1].replace("<name>", firstname_cache)
-                        tts_cache = r.get(txt + "fr")
-                        if tts_cache:
-                            print(f"[{datetime.datetime.now()}] TTS cache getted for: {txt}")
-                        else:
-                            print(f"[{datetime.datetime.now()}] TTS cache not found for: {txt}")
-                            # Generate speech using gTTS and save to a BytesIO object
-                            tts = gTTS(text=txt, lang="fr")
-                            # Create and write the TTS to a BytesIO
-                            mp3_fp = BytesIO()
-                            tts.write_to_fp(mp3_fp)
-                            # Convert the MP3 BytesIO object to WAV format in memory
-                            mp3_fp.seek(0)  # Reset the file pointer to the beginning
-                            r.set(txt + "fr", mp3_fp.read())
-                else:
-                    print(f"Cannot cache TTS for {line.strip()}")
-
-
-print("Successfully getted a token")
-nb_threads = 1
-r = redis.Redis(host='127.0.0.1', port=6379, db=0)
-with open("login.list", "r") as f:
-    file = f.readlines()
-    chunk_size = len(file) // nb_threads
-    threads = []
-    for i in range(nb_threads):
-        start_line = i * chunk_size
-        # Make sure to handle the last chunk properly if it has fewer lines
-        end_line = start_line + chunk_size if i < nb_threads - 1 else len(file)
-        thread = threading.Thread(target=api_call, args=(start_line, end_line))
-        # threads.append(thread)
-        time.sleep(2)
-        thread.start()
-    for thread in threads:
-        thread.join()
+if __name__ == "__main__":
+    conf = Conf("config.ini")
+    r = redis.Redis(host='127.0.0.1', port=6379, db=0)
+    messagesTTSconf = conf.getWelcome() + conf.getGoodbye()
+    with open("firstname.list", "r") as f:
+        for line in f.readlines():
+            # Func to get firstname and append to list
+            api_call(line.strip(), messagesTTSconf)
