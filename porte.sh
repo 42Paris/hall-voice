8610003d45df297e0070c711d3fa1dd3bd03cd3c
julien <jusylvai@42paris.fr>
2024-04-10 05:41:23 +0200

    New porte.sh

--- porte.sh
+++ porte.sh
@@ -1,24 +1,6 @@
 #!/bin/sh
 
-cd /home/bocal/porte42/
-
-ps aux | grep python | grep ./porte.py | awk '{print $2}' | xargs kill
-
-./play.sh ./mp3/sipass.mp3
-
-sleep 5
-
-/usr/bin/python ./porte.py entree-1 welcome &
-sleep 1
-
-/usr/bin/python ./porte.py entree-2 welcome &
-sleep 1
-
-/usr/bin/python ./porte.py sortie-1 goodbye &
-sleep 1
-
-/usr/bin/python ./porte.py sortie-2 goodbye &
-
-# already laucnhed by supervisord
-#cd /root/porte42-api/
-#/usr/bin/python ./app.py
+echo "Kill remaining hallvoice process"
+ps aux | grep python | grep porte.py | awk '{print $2}' | xargs kill
+echo "Launching Hallvoice, Hello!"
+python3 /home/bocal/hall-voice/src/porte.py
