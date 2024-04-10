97fddd93f843497e4ce8546d0a5a10e99bad8935
julien <jusylvai@42paris.fr>
2024-04-10 06:45:36 +0200

    People now have to be in specific network to purge cache

--- src/purgeAPI.py
+++ src/purgeAPI.py
@@ -1,5 +1,6 @@
 import redis
 import urllib.parse
+import ipaddress
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from Conf import Conf
 
@@ -8,34 +9,38 @@ r = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if
-        # Parse query parameters
-        query_components = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
-        # Check if 'purge' parameter exists
-        purge_value = query_components.get('purge', None)
+        client_address = ipaddress.ip_address(self.client_address[0])
+        if (client_address in ipaddress.ip_network("10.42.0.0/16")
+                or (client_address in ipaddress.ip_network("10.43.0.0/16"))
+                or (client_address in ipaddress.ip_network("10.51.0.0/16"))
+                or (client_address in ipaddress.ip_network("127.0.0.1/32"))):
+            # Parse query parameters
+            query_components = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+            # Check if 'purge' parameter exists
+            purge_value = query_components.get('purge', None)
 
-        if purge_value:
-            print(f"Purge request received with value: {purge_value[0]}")
-            cache = r.get(f"login: {purge_value[0]}")
-            if cache:
-                r.delete(f"login: {purge_value[0]}")
-                # Respond to the client
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(f"Login {purge_value[0]} found in cache, purging...".encode())
+            if purge_value:
+                print(f"Purge request received with value: {purge_value[0]}")
+                cache = r.get(f"login: {purge_value[0]}")
+                if cache:
+                    r.delete(f"login: {purge_value[0]}")
+                    # Respond to the client
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(f"Login {purge_value[0]} found in cache, purging...".encode())
+                else:
+                    self.send_response(404)
+                    # If login not found in cache
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(f"Login not found: {purge_value[0]} cannot purge from cache".encode())
             else:
-                self.send_response(404)
-                # If login not found in cache
+                # If no purge parameter, send man
+                self.send_response(400)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                self.wfile.write(f"Login not found: {purge_value[0]} cannot purge from cache".encode())
-        else:
-            # If no purge parameter, send man
-            self.send_response(400)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write("Please provide a 'purge' query parameter.".encode())
+                self.wfile.write("Please provide a 'purge' query parameter.".encode())
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
