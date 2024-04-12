ef57881cc831c070613191e3fa180b6ca9d81701
julien <jusylvai@42paris.fr>
2024-04-12 15:02:43 +0200

    Try except when error on mp3 improve playMP3() and playError()

--- src/Messages.py
+++ src/Messages.py
@@ -1,11 +1,12 @@
 import datetime
 import os
 import json
+
+import pygame
 import redis
 from io import BytesIO
 from random import choice, randint
 from gtts import gTTS, gTTSError
-from pygame import mixer
 
 
 class Messages(object):
@@ -15,7 +16,7 @@ class Messages(object):
         self.goodbyeMsg = conf.getGoodbye()
         self.buildingName = conf.getBuilding()
         self.api = api
-        mixer.init()
+        pygame.mixer.init()
 
     def processMessage(self, msg: str) -> None:
         data = json.loads(msg)
@@ -46,13 +47,18 @@ class Messages(object):
                 j = json.loads(custom_file.read())
                 if kind in j:
                     if "mp3" in j[kind]:
-                        if os.path.isdir("mp3/" + j[kind]["mp3"]) is True:
-                            mixer.music.load("mp3/" + j[kind]["mp3"] + "/" + choice(os.listdir("mp3/" + j[kind]["mp3"])))
-                        elif os.path.isfile("mp3/" + j[kind]["mp3"]) is True:
-                            mixer.music.load("mp3/" + j[kind]["mp3"])
-                        mixer.music.play()
-                        while mixer.music.get_busy():
-                            pass
+                        try:
+                            if os.path.isdir("mp3/" + j[kind]["mp3"]) is True:
+                                pygame.mixer.music.load("mp3/" + j[kind]["mp3"] + "/" + choice(os.listdir("mp3/" + j[kind]["mp3"])))
+                            elif os.path.isfile("mp3/" + j[kind]["mp3"]) is True:
+                                pygame.mixer.music.load("mp3/" + j[kind]["mp3"])
+                            pygame.mixer.music.play()
+                            while pygame.mixer.music.get_busy():
+                                pass
+                        except pygame.error as e:
+                            print(f"[{datetime.datetime.now()}] Error while plying a custom song:\n{e}")
+                            self.playError()
+
                     elif "txt" in j[kind]:
                         lang = j[kind]["lang"] if "lang" in j[kind] else "fr"
                         if isinstance(j[kind]["txt"], list):
@@ -80,7 +86,6 @@ class Messages(object):
             if cache:  # If TTS is cached, play it
                 print(f"[{datetime.datetime.now()}] TTS cache getted!")
                 mp3_fp.write(cache)
-                mp3_fp.seek(0)  # Reset the file pointer to the beginning
                 self.playMP3(mp3_fp)
             else:  # If TTS is NOT cached, cache it AND play it...
                 print(f"[{datetime.datetime.now()}] TTS cache not found, putting in cache")
@@ -93,32 +98,36 @@ class Messages(object):
                     # Convert the MP3 BytesIO object to WAV format in memory
                     mp3_fp.seek(0)  # Reset the file pointer to the beginning
                     self.redis.set(txt+lang, mp3_fp.read())
-                    mp3_fp.seek(0)  # Reset the file pointer to the beginning, again...
                     self.playMP3(mp3_fp)
                 except gTTSError as e:  # If we break gTTS API with rate-limit
                     print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS error:\n{e}")
-        else:
-            print(f"[{datetime.datetime.now()}] Cannot TTS, var txt is None")
-            cache = self.redis.get("HallvoiceERROR")
-            if cache:
-                print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS cached")
-                self.playMP3(cache)
-            else:
-                print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS not cached, caching him")
-                try:
-                    tts = gTTS(text="Hallvoice Error, please contact staff member", lang="en")
-                    tts.write_to_fp(mp3_fp)
-                    # Convert the MP3 BytesIO object to WAV format in memory
-                    mp3_fp.seek(0)  # Reset the file pointer to the beginning
-                    self.redis.set("HallvoiceERROR", mp3_fp.read())
-                    mp3_fp.seek(0)
                     self.playMP3(mp3_fp)
-                except gTTSError as e:
-                    print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS error:\n{e}")
+        else:
+            self.playError()
 
     @staticmethod
-    def playMP3(mp3) -> None:
-        mixer.music.load(mp3)
-        mixer.music.play()
-        while mixer.music.get_busy():
+    def playMP3(mp3: BytesIO) -> None:
+        mp3.seek(0)  # Reset the file pointer to the beginning
+        pygame.mixer.music.load(mp3)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
             pass
+
+    def playError(self) -> None:
+        mp3_fp = BytesIO()
+        cache = self.redis.get("HallvoiceERROR")
+        if cache:
+            print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS cached")
+            mp3_fp.write(cache)
+            self.playMP3(mp3_fp)
+        else:
+            print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS not cached, caching him")
+            try:
+                tts = gTTS(text="Hallvoice Error, please contact staff member", lang="en")
+                tts.write_to_fp(mp3_fp)
+                # Convert the MP3 BytesIO object to WAV format in memory
+                mp3_fp.seek(0)  # Reset the file pointer to the beginning
+                self.redis.set("HallvoiceERROR", mp3_fp.read())
+                self.playMP3(mp3_fp)
+            except gTTSError as e:
+                print(f"[{datetime.datetime.now()}] HallvoiceERROR TTS error:\n{e}")
