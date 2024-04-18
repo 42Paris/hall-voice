09866c6d5041051b7e2f5dcefac752c18b503a93
julien <jusylvai@42paris.fr>
2024-04-18 12:59:37 +0200

    improved main

--- src/porte.py
+++ src/porte.py
@@ -1,22 +1,30 @@
 import datetime
 import sys
+import os
 from Conf import Conf
 from API42 import API42
 from Kafka import Kafka
 
-
 if __name__ == "__main__":
-    if sys.argv[1] is not None:
-        try:
-            with open(f"logs/{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}_hallvoice.log", "w", buffering=1) as f:
-                if '--log-in-file' in sys.argv:
-                    sys.stdout = f
-                    sys.stderr = f
-                conf = Conf(sys.argv[1])
-                api = API42(conf)
-                consumer = Kafka(conf, api)
-                consumer.consume_messages()
-        except FileNotFoundError as err:
-            print(f"Error reading/opening configuration file: {err}")
-    else:
+    if len(sys.argv) < 2:
         print("Where conf file? Use me like this!\n\tpython3 porte.py config.yaml")
+        exit(1)
+    elif "--help" in sys.argv:
+        print("Usage: python3 src/porte.py config.yaml")
+        print("\tYou can also add --log-in-file in the end to output log in file")
+        exit(0)
+    elif sys.argv[1] is not None and os.path.exists(sys.argv[1]):
+        with open(f"logs/{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}_hallvoice.log", "w", buffering=1) as f:
+            if '--log-in-file' in sys.argv:
+                sys.stdout = f
+                sys.stderr = f
+            conf = Conf(sys.argv[1])
+            api = API42(conf)
+            consumer = Kafka(conf, api)
+            consumer.consume_messages()
+    else:
+        print("Config path not exists, check your args...")
+        print("Path for config file should be the first arg")
+        print("Usage: python3 porte.py config.yaml")
+        print("\tYou can also add --log-in-file in the end to output log in file")
+        exit(1)
